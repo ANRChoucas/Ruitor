8aeec4a3d698ffe30cda68f6487e09990f8d77e4
mbunel <mattiabunel@hotmail.fr>
2019-06-21 19:42:53 +0200

    Definition classe fonctions floues

---
+++ src/fuzzyUtils/FuzzyficationFunction.py
@@ -0,0 +1,25 @@
+class FuzzyficationFunction(list):
+
+    ftypes = {
+        2: 'linear',
+        3: 'triangular',
+        4: 'trapezoidal'
+    }
+    
+    def __init__(self, *args):
+        super().__init__(*args)
+        sorted(self, key=lambda x: x[0])
+
+        self.ftype = self.ftypes[len(self)]
+
+    def to_linear(self):
+        pass
+
+    def to_triangular(self):
+        pass
+
+    def to_trapezoidal(self):
+        pass
+
+    def to_crisp(self):
+        pass
