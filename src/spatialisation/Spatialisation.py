ca4ecbb87cd212af83c9611255375796cc33c82b
mbunel <mattiabunel@hotmail.fr>
2019-10-31 18:12:17 +0100

    Extraction Spatialisation element

--- src/spatialisation/Spatialisation.py
+++ src/spatialisation/Spatialisation.py
@@ -2,151 +2,18 @@
 Spatialisation
 """
 
+
 import numpy as np
-
-from pathos.multiprocessing import ProcessingPool as Pool
-from functools import reduce
-from itertools import groupby
-from operator import itemgetter
 from more_itertools import chunked
-
 from rasterio import features
 from rasterio.windows import Window
 
 from fuzzyUtils import FuzzyRaster
-from .Metric import Cell_Distance, Distance, Metric, Nothing, Angle
-from .Selector import Selector, Selector2
 
-
-class SpatialisationElement:
-    """
-    Classe spatialisationElement
-    """
-
-    def __init__(self, context, raster, metric, selector):
-        self.context = context
-        self.raster = raster
-        # Initialisation des objets Metric et Selector
-        self._init_metric(metric)
-        self._init_selector(selector)
-
-    def _init_metric(self, metric=Angle):
-        self.metric = metric(self)
-
-    def _init_selector(self, selector=Selector):
-        self.selector = selector(self)
-
-    def compute(self, *args):
-        tmp = self.metric.compute()
-        self.selector.compute(tmp)
-        return tmp
-
-
-class SpatialisationElementSequence(dict):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def _get_partial(self, key, pos):
-        for k in self.keys():
-            if k[pos] == key:
-                yield self[k]
-
-    def _elem_reduce(self, function, values, pools=6, chuncks=12):
-        return reduce(function, values)
-        # Ne marche pas a refaire
-        # _tempRes = []
-        # p = Pool(processes=pools)
-        # def _par_reduce(pool, function, values):
-        #     def fun(x): return reduce(function, x)
-        #     res = p.map(fun, chuncked(values, chuncks))
-        #     return res
-        # _tempRes.append(_par_reduce(p, function, valusq))
-        # if len(_tempRes) > chuncks:
-        #     _par_reduce(p, function, _tempRes)
-        # else:
-        #     return reduce(function, _tempRes)
-
-    def element_compute(self, element):
-        return element.compute()
-
-    def _agg_objects(self, agg):
-        res = self._elem_reduce(lambda x, y: x | y, agg.values())
-
-        if __debug__:
-            print("agg_obj : Done")
-
-        return res
-
-    def _agg_objects_part(self, agg):
-        """
-        """
-        res = {}
-        keysList = list(agg.keys())
-        keysList.sort(key=itemgetter(0))
-
-        grouper = groupby(keysList, key=itemgetter(0))
-        for gr in grouper:
-            val = itemgetter(*gr[1])(agg)
-            try:
-                res[gr[0]] = self._elem_reduce(lambda x, y: x | y, val)
-            except TypeError:
-                res[gr[0]] = val
-
-        if __debug__:
-            print("agg_obj_part : Done")
-
-        return res
-
-    def _agg_spa_rel(self, agg):
-        """
-        """
-        res = {}
-
-        keysList = list(agg.keys())
-        keysList.sort(key=itemgetter(0, 1))
-
-        grouper = groupby(keysList, key=itemgetter(0, 1))
-        for gr in grouper:
-            val = itemgetter(*gr[1])(agg)
-            try:
-                res[gr[0]] = self._elem_reduce(lambda x, y: x & y, val)
-            except TypeError:
-                res[gr[0]] = val
-
-        if __debug__:
-            print("agg_spa_rel : Done")
-
-        return res
-
-    def compute(self, pools=6):
-        """
-        Fonction pour l'initialisation du calcul
-        """
-
-        # Définition pool pour traitement //
-        # des rasters
-        sp_list = list(zip(*self.items()))
-
-        with Pool(processes=pools) as t:
-            cmp_res = t.map(self.element_compute, sp_list[1])
-
-        cmp_dic = {k: v for k, v in zip(sp_list[0], cmp_res)}
-        zou = self._agg_spa_rel(cmp_dic)
-        zi = self._agg_objects_part(zou)
-        zu = self._agg_objects(zi)
-
-        if __debug__:
-            print("writing tempfiles")
-            for k, v in cmp_dic.items():
-                f_name = "obj%s_part%s_rel%s" % k
-                v.write("./_outTest/%s.tif" % f_name)
-
-            for k, v in zou.items():
-                f_name = "obj%s_part%s" % k
-                v.write("./_outTest/%s.tif" % f_name)
-
-        return zu
+from .Metric import Angle, Cell_Distance, DAlt, Distance, Metric, Nothing
+from .Selector import SelectorNull, SelectorX, SelectorX2, SelectorX3
+from .SpatialisationElement import (SpatialisationElement,
+                                    SpatialisationElementSequence)
 
 
 class Spatialisation:
@@ -156,7 +23,7 @@ class Spatialisation:
     Destinée à modéliser UN élément de localisation
     """
 
-    def __init__(self, parameters, raster, raster2):
+    def __init__(self, parameters, raster):
         """
         Fonction d'initialisation de la classe Spatialisation
         """
@@ -173,8 +40,6 @@ class Spatialisation:
 
         # Création du raster flou résultat
         self.raster = FuzzyRaster(raster=raster, window=self.zir)
-        self.raster2 = FuzzyRaster(raster=raster2[0], window=self.zir)
-        self.raster3 = FuzzyRaster(raster=raster2[1], window=self.zir)
 
         self.indice_parsing(self.params['indices'])
 
@@ -188,31 +53,21 @@ class Spatialisation:
         indice = indice[0]
 
         site_counter = 0
-        for s in indice['site']:
-            zi = np.zeros_like(self.raster.values)
 
-            features.rasterize(
-                [s], out=zi,
-                transform=self.raster.raster_meta['transform'],
-                all_touched=True,
-                dtype=self.raster.raster_meta['dtype'])
+        # tmpdir = mkdtemp()
 
-            fuzz = FuzzyRaster(array=zi, meta=self.raster.raster_meta)
+        for geom in indice['site']:
+            # filename = path.join(tmpdir, 'obj_%s.dat' % site_counter)
+            # zi = np.memmap(filename, mode='w+', dtype=self.raster.values.dtype, shape=self.raster.values.shape)
 
-            self.spaElms[(site_counter, 0, 0)
-                         ] = SpatialisationElement(self, fuzz, metric=Cell_Distance, selector=Selector2)
-            self.spaElms[(site_counter, 0, 1)
-                         ] = SpatialisationElement(self, fuzz, metric=Distance, selector=Selector2)
+            self.spaElms[(site_counter, 0, 0)] = SpatialisationElement(
+                self, geom, metric=Angle, selector=SelectorX2)
+            self.spaElms[(site_counter, 0, 1)] = SpatialisationElement(
+                self, geom, metric=Distance, selector=SelectorX)
+            self.spaElms[(site_counter, 0, 2)] = SpatialisationElement(
+                self, geom, metric=DAlt, selector=SelectorX3, values_raster=self.raster)
 
             site_counter += 1
-
-        # aa = SpatialisationElement(self, self.raster)
-        # bb = SpatialisationElement(self, self.raster2)
-        # cc = SpatialisationElement(self, self.raster3)
-
-        # self.spaElms[(0, 0, 0)] = aa
-        # self.spaElms[(1, 0, 0)] = bb
-        # self.spaElms[(2, 0, 0)] = cc
 
     def site_parsing(self, site):
         pass
---
+++ src/spatialisation/SpatialisationElement.py
@@ -0,0 +1,82 @@
+
+
+import numpy as np
+from rasterio import features
+
+import config
+from fuzzyUtils import FuzzyRaster
+
+from .AggregatorStrategies import FirstAggragator
+
+
+class SpatialisationElement:
+    """
+    Classe spatialisationElement
+    """
+
+    def __init__(self, context, geom, metric, selector, *args, **kwargs):
+        self.context = context
+        self.geom = geom
+
+        # Initialisation des objets Metric et Selector
+        self._init_metric(metric, *args, **kwargs)
+        self._init_selector(selector, *args, **kwargs)
+
+    def _init_metric(self, metric, *args, **kwargs):
+        self.metric = metric(self, *args, **kwargs)
+
+    def _init_selector(self, selector, *args, **kwargs):
+        self.selector = selector(self, *args, **kwargs)
+
+    def rasterise(self):
+
+        zi = np.zeros_like(self.context.raster.values)
+
+        features.rasterize(
+            [self.geom], out=zi,
+            transform=self.context.raster.raster_meta['transform'],
+            all_touched=True,
+            dtype=self.context.raster.raster_meta['dtype'])
+
+        # FuzzyRaster(array=zi, meta=self.context.raster.raster_meta)
+        return zi
+
+    def compute(self, *args):
+        # Rasterisation
+        geom_raster = self.rasterise()
+        tmp = self.metric.compute(geom_raster)
+        # Fuzzyfication
+        self.selector.compute(tmp)
+
+        if config.log['verbosity'] >= 3:
+            print("Element compute : %s " % self.metric.__class__)
+
+        return tmp
+
+
+class SpatialisationElementSequence(dict):
+
+    default_aggregator_strategy = FirstAggragator
+
+    def __init__(self, aggregator_strategy=None, *args, **kwargs):
+
+        if aggregator_strategy:
+            self.aggregator_strategy = aggregator_strategy(self)
+        else:
+            self.aggregator_strategy = self.default_aggregator_strategy(self)
+
+        super().__init__(*args, **kwargs)
+
+    def _get_partial(self, key, pos):
+        for k in self.keys():
+            if k[pos] == key:
+                yield self[k]
+
+    def element_compute(self, element):
+        return element.compute()
+
+    def compute(self, pools=6):
+        """
+        Fonction pour l'initialisation du calcul
+        """
+        return self.aggregator_strategy.compute()
