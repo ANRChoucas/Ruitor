94b8640dbbd45ade8a5eb1299f2b119fbf3a6cca
mbunel <mattiabunel@hotmail.fr>
2019-10-31 18:11:48 +0100

    Ajout options config

--- src/config.py
+++ src/config.py
@@ -1,11 +1,22 @@
 data = {
     'MNT': {
-        "path": "/home/mbunel/Données/RGEALTI/RGEALTI_2-0_5M_ASC_LAMB93-IGN69_D038_2017-05-20/",
-        "name": "RGEALTI_38_test.vrt"
+        # "path": "/home/mbunel/Données/RGEALTI/RGEALTI_2-0_5M_ASC_LAMB93-IGN69_D038_2017-05-20/",
+        # "name": "RGEALTI_38_test.vrt",
+        "path": "/home/mbunel/Documents/Code/Ruitor/data/mnt/",
+        "name": "BR.tif"
     }
 }
 
 proxy = {
     'url': "http://proxy.ign.fr",
     'port': 3128
+}
+
+multiprocessing = {
+    'pools': 6
+}
+
+log = {
+    'verbosity': 2,
+    'int_file': False
 }
