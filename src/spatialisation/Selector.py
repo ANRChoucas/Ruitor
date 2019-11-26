2687ae2f6b4c3b623202be40a783ece72d7ea677
mbunel <mattiabunel@hotmail.fr>
2019-11-26 19:29:36 +0100

    Ajout Sélecteurs

--- src/spatialisation/Selector.py
+++ src/spatialisation/Selector.py
@@ -19,7 +19,7 @@ class Selector:
         return self._compute_ff_params(*args)
 
     def _compute_ff_params(self, *args):
-        raise EnvironmentError
+        raise NotImplementedError
 
 
 class SelectorNull(Selector):
@@ -30,12 +30,57 @@ class SelectorNull(Selector):
         pass
 
 
+class CompareVal(Selector):
+    def __init__(self, context, value, *args, **kwargs):
+        self.value = value
+        super().__init__(context, *args, **kwargs)
+
+    def _compute_ff_params(self, *args):
+        raise NotImplementedError
+
+
+class InfVal(CompareVal):
+    def __init__(self, context, value, *args, **kwargs):
+        super().__init__(context, value, *args, **kwargs)
+
+    def _compute_ff_params(self, *args):
+        bsup = self.value + 10
+        return [(self.value, 1.0), (bsup, 0.0)]
+
+
+class SupVal(CompareVal):
+    def __init__(self, context, value, *args, **kwargs):
+        super().__init__(context, value, *args, **kwargs)
+
+    def _compute_ff_params(self, *args):
+        binf = self.value - 10
+        return [(binf, 0.0), (self.value, 1.0)]
+
+
+class EqVal(CompareVal):
+    def __init__(self, context, value, *args, **kwargs):
+        super().__init__(context, value, *args, **kwargs)
+
+    def _compute_ff_params(self, *args):
+        binf = self.value - 10
+        bsup = self.value + 10
+        return [(binf, 0.0), (self.value, 1.0), (bsup, 0.0)]
+
+
 class SelectorX(Selector):
     def __init__(self, context, *args, **kwargs):
         super().__init__(context, *args, **kwargs)
 
     def _compute_ff_params(self, *args):
-        return [(5, 1.0), (25, 0.0)]
+        return [(25, 1.0), (125, 0.0)]
+
+
+class SelectorX1(Selector):
+    def __init__(self, context, *args, **kwargs):
+        super().__init__(context, *args, **kwargs)
+
+    def _compute_ff_params(self, *args):
+        return [(0, 0.0), (2, 1.0)]
 
 
 class SelectorX2(Selector):
@@ -43,7 +88,7 @@ class SelectorX2(Selector):
         super().__init__(context, *args, **kwargs)
 
     def _compute_ff_params(self, *args):
-        return [(160, 0.0), (180, 1.0), (200, 0.0)]
+        return [(0, 1.0), (2, 0.0)]
 
 
 class SelectorX3(Selector):
