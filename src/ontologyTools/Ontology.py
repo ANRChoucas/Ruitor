62299129f547e37938d59d5135740e1cf31ad5bc
mbunel <mattiabunel@hotmail.fr>
2019-12-03 19:58:48 +0100

    Correction Typos

--- src/ontologyTools/Ontology.py
+++ src/ontologyTools/Ontology.py
@@ -52,15 +52,17 @@ class SROnto(Ontology):
         try:
             rsas = self.get_atomic_spatial_relation(spatial_relation)
         except ValueError:
-            rsas = spatial_relation,
+            rsas = (spatial_relation,)
 
         for rsa in rsas:
             metric = self.get_metric(rsa)
             selector = self.get_selector(rsa)
             outDic[rsa.name] = {"metric": metric, "selector": selector}
 
-            logger.debug("\n%s \n\t metric : %s\n\t selector : %s" % (rsa, metric["name"], selector["name"]))
-            #logger.debug("\t%s selector : %s" % (rsa, selector["name"]))
+            logger.debug(
+                "%s, metric : %s, selector : %s"
+                % (rsa, metric["name"], selector["name"])
+            )
         return outDic
 
     def get_atomic_spatial_relation(self, spatial_relation):
