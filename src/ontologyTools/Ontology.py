205c05aa680120f371e114a6eb9bad450dce579b
mbunel <mattiabunel@hotmail.fr>
2019-12-12 16:22:19 +0100

    Nettoyage code Ontology

--- src/ontologyTools/Ontology.py
+++ src/ontologyTools/Ontology.py
@@ -6,7 +6,7 @@ import logging
 import ast
 from owlready2 import get_ontology
 
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
 
 class Ontology:
@@ -17,40 +17,48 @@ class Ontology:
     def __init__(self, path):
         self.ontho = get_ontology(path)
         self.ontho.load()
-        logger.info("ontology namespace: %s" % self.ontho.base_iri)
+        LOGGER.info("ontology namespace: %s", self.ontho.base_iri)
 
     def __getattr__(self, name):
         return self.ontho.__getattr__(name)
 
     def get_from_iri(self, uri):
+        """
+        """
         try:
             return self.ontho.search_one(iri=uri)
-        except:
-            logger.debug("%s unknown" % (uri,))
+        except ValueError:
+            LOGGER.debug("%s unknown", uri)
 
     def _pythonAnotationParsing(self, owlcls):
         """
         """
-        pythonName = owlcls.pythonName[0]
+        python_name = owlcls.pythonName[0]
         try:
-            pythonParameter = ast.literal_eval(owlcls.pythonParameter[0])
+            python_parameter = ast.literal_eval(owlcls.pythonParameter[0])
         except IndexError:
-            pythonParameter = {}
-        outDic = {"name": pythonName, "kwargs": pythonParameter}
+            python_parameter = {}
+        out_dic = {"name": python_name, "kwargs": python_parameter}
 
-        return outDic
+        return out_dic
 
 
 class SROnto(Ontology):
+    """
+    Classe SROnto
+    """
+
     def __init__(self, path):
-        logger.debug("SRO ontology made")
+        LOGGER.debug("SRO ontology made")
         super().__init__(path)
 
     def treat_spatial_relation(self, spatial_relation):
+        """
+        """
 
-        logger.info("%s : extraction" % (spatial_relation,))
+        LOGGER.info("%s : extraction", spatial_relation)
 
-        outDic = {}
+        out_dic = {}
 
         try:
             rsas = self.get_atomic_spatial_relation(spatial_relation)
@@ -60,67 +68,71 @@ class SROnto(Ontology):
         for rsa in rsas:
             metric = self.get_metric(rsa)
             selector = self.get_selector(rsa)
-            outDic[rsa.name] = {"metric": metric, "selector": selector}
+            out_dic[rsa.name] = {"metric": metric, "selector": selector}
 
-            logger.debug(
-                "%s, metric : %s, selector : %s"
-                % (rsa, metric["name"], selector["name"])
-            )
-        return outDic
+            LOGGER.debug(
+                "%s, metric : %s, selector : %s", rsa, metric["name"], selector["name"])
+        return out_dic
 
     def get_atomic_spatial_relation(self, spatial_relation):
+        """
+        """
         rsa = spatial_relation.hasRelationSpatialeAtomique
         if not rsa:
-            raise ValueError(
-                "No hasRelationSpatialeAtomique for %s" % (spatial_relation,)
-            )
+            LOGGER.error("No hasRelationSpatialeAtomique for %s",
+                         spatial_relation)
+            raise ValueError
         return rsa
 
     def get_modifier(self, modifier):
+        """
+        """
         return self._pythonAnotationParsing(modifier)
 
     def _add_parameter(self, obj, dic):
-        parametersDic = {**dic}
+        parameters_dic = {**dic}
 
         try:
-            metricParameter = self.get_parameter(obj)
-            parametersDic.update({"parameters": metricParameter})
+            metric_parameter = self.get_parameter(obj)
+            parameters_dic.update({"parameters": metric_parameter})
         except AttributeError:
             pass
 
-        return parametersDic
+        return parameters_dic
 
     def get_metric(self, atomic_spatial_relation):
-        metricList = atomic_spatial_relation.hasMetric
-        metricDic = self._pythonAnotationParsing(metricList)
-        metricOutDic = self._add_parameter(metricList, metricDic)
-        return metricOutDic
+        """
+        """
+        metric_list = atomic_spatial_relation.hasMetric
+        metric_dic = self._pythonAnotationParsing(metric_list)
+        metric_out_dic = self._add_parameter(metric_list, metric_dic)
+        return metric_out_dic
 
     def get_selector(self, atomic_spatial_relation):
-        selectorList = atomic_spatial_relation.hasSelector
-        selectorDic = self._pythonAnotationParsing(selectorList)
-        selectorOutrDic = self._add_parameter(selectorList, selectorDic)
-        return selectorOutrDic
+        """
+        """
+        selector_list = atomic_spatial_relation.hasSelector
+        selector_dic = self._pythonAnotationParsing(selector_list)
+        selector_outr_dic = self._add_parameter(selector_list, selector_dic)
+        return selector_outr_dic
 
     def get_parameter(self, python_object):
-        prmList = python_object.hasParameter
-
-        outList = []
-        for i in prmList:
+        """
+        """
+        prm_list = python_object.hasParameter
+        out_list = []
+        for i in prm_list:
             anot = self._pythonAnotationParsing(i)
             uri = i.get_iri(i)
-
-            try:
-                tp = i.pythonType[0]
-            except IndexError:
-                tp = None
-
-            outList.append({"uri": uri, "type": tp, **anot})
-
-        return outList
+            out_list.append({"uri": uri, **anot})
+        return out_list
 
 
 class ROOnto(Ontology):
+    """
+    Classe ROOnto
+    """
+
     def __init__(self, path):
-        logger.debug("ROO ontology made")
+        LOGGER.debug("ROO ontology made")
         super().__init__(path)
